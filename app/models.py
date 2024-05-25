d9e8e0732f7352dac0618ac14b568c770d4245c3
Blake Calhoun <blakecalhoun@tutanota.com>
2024-05-25 11:22:47 -0400

    first scripts for main directory

---
+++ app/models.py
@@ -0,0 +1,23 @@
+# Models to store file information.
+# Imports
+from app import db
+from flask_login import UserMixin
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    songs = db.relationship('Song', backref='author', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}')"
+
+class Song(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    file_path = db.Column(db.String(100), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Song('{self.title}', '{self.file_path}')"
